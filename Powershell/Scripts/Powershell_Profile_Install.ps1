c43373cdb0aab1f4386d64ba3e9e3810d2c6ef7c
Morten Lund <92morten@gmail.com>
2019-02-03 13:20:05 +0100

    Added powershell installer from nextgen branch

---
+++ Powershell/Scripts/Powershell_Profile_Install.ps1
@@ -0,0 +1,25 @@
+﻿# Check administrator
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Output "Not running as administrator.."
+    Exit 1
+}
+
+# == LINK FIRST ==
+#
+# Install Profile.ps1
+# Create link to Configs\WindowsPowerShell directory
+function CreateLink($symlink, $targetFile) { cmd /c mklink /J $($symlink) $($targetFile) }
+CreateLink("$($HOME)\Documents\WindowsPowerShell", "$(pwd)\Configs\WindowsPowerShell")
+
+# =============== Install modules ===============
+
+# Update package provider
+Install-PackageProvider -Name NuGet -Force
+
+# Install oh-my-posh modules
+Install-Module posh-git -Force
+Install-Module oh-my-posh -Force
+
+# Install PSFzf modules
+Install-Module PSFzf -Force
