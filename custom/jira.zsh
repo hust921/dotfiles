c5b957e70cd638c9d69816bbf006e00b86988982
Morten Lund <92morten@gmail.com>
2019-12-06 14:21:19 +0100

    Added Jira fzf for zsh

---
+++ custom/jira.zsh
@@ -0,0 +1,42 @@
+#!/usr/bin/env zsh
+# Taken from: https://github.com/knpwrs/dotfiles/blob/65ab46da3e46ad44ff5e9a585b1b784c83d1e74e/lib/jira.zsh
+
+# You will need the `jq` and `fzf` commands installed in order for these
+# functions to work:
+#
+#   - https://stedolan.github.io/jq/
+#   - https://github.com/junegunn/fzf
+#
+# You will also need to define $JIRA_TEAM, $JIRA_USER, and $JIRA_TOKEN
+# environment variables. See the following for information on setting
+# machine-private environment variables:
+#
+#   - https://github.com/knpwrs/dotfiles#private-environment-variables
+#
+# See the following for information on creating a JIRA access token:
+#
+#  - https://confluence.atlassian.com/cloud/api-tokens-938839638.html ( https://archive.is/vjoEZ )
+#
+
+function jql() {
+  curl "https://$JIRA_TEAM.atlassian.net/rest/api/2/search?jql=$1&maxResults=100&startAt=$(($2 * 100))" \
+    -s -u "$JIRA_USER:$JIRA_TOKEN" \
+    |  jq -r '.issues[] | "\(.key): \(.fields.summary)"'
+}
+
+function fjql() {
+  source ~/dotfiles/zshconfig/jira_creds.zsh
+  ticket=$({for ((req=0;req<5;req++)); do
+    jql $1 $req &
+  done} | fzf --no-hscroll +m | cut -d ':' -f 1)
+
+  [ ${#ticket} -gt 0 ] && open "https://$JIRA_TEAM.atlassian.net/browse/$ticket"
+}
+
+alias fj="fjql 'order%20by%20updated%20DESC'"
+alias fjm="fjql 'assignee%20%3D%20currentUser()%20AND%20resolution%20%3D%20Unresolved%20order%20by%20updated%20DESC'"
+
+function jo() {
+  local ticket=$(git branch | grep \* | cut -d ' ' -f 2 | cut -d '/' -f 1)
+  open "https://$JIRA_TEAM.atlassian.net/browse/$ticket"
+}
--- install_nvim.sh
+++ install_nvim.sh
@@ -6,11 +6,11 @@ set -e
 # Assumming apt-get
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update -y
-sudo apt install neovim python3-neovim python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y neovim python-dev python-pip python3-dev python3-pip
 pip3 install pynvim jedi flake8
 
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir ~/.config
+mkdir -p ~/.config
 ln -s ~/dotfiles/config/nvim ~/.config/nvim
 nvim +PlugInstall +qall
