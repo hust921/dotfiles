ba128e494fc99c3d926b2879593bb3ec7a4611fd
Morten Lund <mlairsup@outlook.com>
2020-09-24 09:25:33 +0200

    Added wsltools.zsh & moved win32yank. Added powershell and expl aliases

---
+++ custom/wsltools.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+# If not running WSL => exit
+if grep -iv "microsoft" /proc/version >> /dev/null; then
+    exit 0
+fi
+
+# Windows Aliases
+alias wi="win32yank.exe -i"
+alias wo="win32yank.exe -o"
+alias expl='/mnt/c/Windows/explorer.exe $(wslpath -w $(pwd))'
+
+# Run Powershell from WSL
+alias pwsh='/mnt/c/Program\ Files/PowerShell/6/pwsh.exe'
+powershell()
+{
+    if [[ $# -eq 0 ]];then
+        pwsh
+    else
+        winpath="$(wslpath -w "$1")"
+        echo "wslpath: $winpath"
+        pwsh -noexit -command "cd $winpath"
+    fi
+}
