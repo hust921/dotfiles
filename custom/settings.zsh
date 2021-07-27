01d927bcf5497baf351899d0c00656882b9dc4b9
Morten Lund <92morten@gmail.com>
2021-07-27 10:45:51 +0200

    Use fd-find as FZF engine if both are installed

---
+++ custom/settings.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# If fzf and fd-find do not exist => exit
+! [ -f "$HOME/.fzf.zsh" ] && return 0
+! [ -f "$HOME/.cargo/bin/fd" ] && return 0
+
+# Use fd-find as FZF engine
+export FZF_DEFAULT_COMMAND='fd --type file --color=always'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND='fd --type directory --color=always'
+export FZF_DEFAULT_OPTS="--ansi"
