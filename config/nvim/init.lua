df2f0850638a28ff8abd888a65d49403ae2e2f3a
Morten Lund <92morten@gmail.com>
2022-02-04 13:32:25 +0100

    Added colorscheme. One

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,11 @@
 require 'user.options'
 require 'user.keymaps'
 require 'user.plugins'
+
+-- Set Colorscheme
+local colorscheme = "one"
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
