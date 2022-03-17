a69e3fc5cb8ae71a0ee139e0f6adc66674bef2a1
Morten Lund <92morten@gmail.com>
2022-03-17 10:17:26 +0100

    Added clipboard json & xml formatting

---
+++ clipboard_formatting.zsh
@@ -0,0 +1,17 @@
+#! /usr/bin/zsh
+
+function jjf ()
+{
+    local tmpfile
+    tmpfile=$(mktemp "/tmp/formattet_XXXXXXXXXXX.json")
+    win32yank.exe -o >> "$tmpfile"
+    nvim +FormatJson +w "$tmpfile"
+}
+
+function xxf ()
+{
+    local tmpfile
+    tmpfile=$(mktemp "/tmp/formattet_XXXXXXXXXXX.xml")
+    win32yank.exe -o >> "$tmpfile"
+    nvim +FormatXml +w "$tmpfile"
+}
