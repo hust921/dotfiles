2a29095ed2bdd465c76eda9b1b3b5efca06c1daa
Morten Lund <92morten@gmail.com>
2022-08-08 18:54:09 +0200

    Upgraded plugins

--- config/nvim/lua/user/completion.lua
+++ config/nvim/lua/user/completion.lua
@@ -125,9 +125,6 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
   experimental = {
     ghost_text = false,
     native_menu = false,
