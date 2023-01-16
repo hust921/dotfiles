203ffee5923ff7bdc633aaad9e3387a67b14ffe7
Morten Lund <92morten@gmail.com>
2023-01-16 09:11:50 +0100

    fix code-action & indention

--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
   ensure_installed = { "bash", "c", "c_sharp", "cmake", "comment", "cpp", "css", "devicetree", "dockerfile", "dot", "go", "haskell", "help", "html", "http", "java", "javascript", "json", "latex", "lua", "make", "markdown", "markdown_inline", "ninja", "python", "regex", "rust", "sql", "toml", "typescript", "vim", "yaml" },
-  sync_install = false, 
+  sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopair = {
     enable = true,
@@ -12,5 +12,5 @@ configs.setup {
     additional_vim_regex_highlighting = true,
 
   },
-  indent = { enable = true, disable = { "yaml" } },
+  indent = { enable = false, disable = { "yaml" } },
 }
