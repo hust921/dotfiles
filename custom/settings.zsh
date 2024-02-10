d760a0d1fb62ecb8dee44ae2b373eb108a51109b
Morten Lund <92morten@gmail.com>
2024-02-10 09:44:05 +0100

    Added keybinding terminal zsh shell

--- custom/settings.zsh
+++ custom/settings.zsh
@@ -8,3 +8,12 @@ export FZF_DEFAULT_COMMAND='fd --type file --color=always'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND='fd --type directory --color=always -d 1 -L'
 export FZF_DEFAULT_OPTS="--ansi"
+
+# Control + backspace to delete whole word
+bindkey '^H' backward-kill-word
+# Control + arrows to navigate whole words
+bindkey ";5C" forward-word
+bindkey ";5D" backward-word
+bindkey '^u' undo # Undo usinc C-u
+bindkey '^[e' kill-line # Delete from cursor to end of line
+bindkey '^[w' kill-region # Delete from cursor to start of line
